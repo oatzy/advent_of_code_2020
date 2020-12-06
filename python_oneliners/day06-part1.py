75088dc6ac4488a78fb75beb799ed7d0658bba76
Chris Oates <kryz.oatz@gmail.com>
2020-12-06 11:53:18 +0000

    tweak: rename day06 oneliner

--- python_oneliners/day06-part1.py
+++
@@ -1,5 +0,0 @@
-# part 1
-print(sum(len(set(group.replace('\n', ''))) for group in open('../inputs/day06.txt').read().split('\n\n')))
-
-# part 2
-print(sum(sum([all(c in g for g in group) for c in group[0]]) for group in (groups.split() for groups in open('../inputs/day06.txt').read().split('\n\n'))))
