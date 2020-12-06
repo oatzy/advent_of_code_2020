cebd7ba73db6cae974d71ca4cb3060cac7bfb545
Chris Oates <kryz.oatz@gmail.com>
2020-12-06 19:06:16 +0000

    day02: one-liner tweak

--- python_oneliners/day02.py
+++ python_oneliners/day02.py
@@ -1,8 +1,8 @@
 # part 1
 import re
-print(sum(int(m.group(1)) <= m.group(4).count(m.group(3)) <= int(m.group(2)) for m in (
-    re.match(r"(\d+)\-(\d+) (\w): (\w+)", line) for line in open("../inputs/day02.txt"))))
+print(sum(int(l) <= s.count(c) <= int(h) for (l, h, c, s) in (
+    re.match(r"(\d+)\-(\d+) (\w): (\w+)", line).groups() for line in open("../inputs/day02.txt"))))
 
 # part 2
-print(sum((m.group(4)[int(m.group(1))-1] == m.group(3)) ^ (m.group(4)[int(m.group(2))-1] == m.group(3))
-          for m in (re.match(r"(\d+)\-(\d+) (\w): (\w+)", line) for line in open("../inputs/day02.txt"))))
+print(sum((s[int(l)-1] == c) ^ (s[int(h)-1] == c)
+          for (l, h, c, s) in (re.match(r"(\d+)\-(\d+) (\w): (\w+)", line).groups() for line in open("../inputs/day02.txt"))))
