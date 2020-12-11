a82bab13e870c4518d84e10e2248b1ca9a267e0d
Chris Oates <kryz.oatz@gmail.com>
2020-12-11 18:59:48 +0000

    day11: part 2, still pretty slow
    This is my least favourite solution so far. It's slow, and there's a lot of similar code which could be refactored.
    
    But it does work, so that's something

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,7 +1,42 @@
 use std::collections::HashMap;
 use std::fs;
 
+static STEPS: [(isize, isize); 8] = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+];
+
 type Seats = HashMap<P, Seat>;
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+struct SeatMap {
+    seats: Seats,
+    width: usize,
+    height: usize,
+}
+
+impl SeatMap {
+    fn first_in_path(&self, origin: P, step: &(isize, isize)) -> Seat {
+        let (mut x, mut y) = (origin.0 as isize + step.0, origin.1 as isize + step.1);
+
+        while x >= 0 && y >= 0 && x <= self.width as isize && y <= self.height as isize {
+            let p = P(x as usize, y as usize);
+            if self.seats.contains_key(&p) {
+                return *self.seats.get(&p).unwrap();
+            }
+            x += step.0;
+            y += step.1;
+        }
+
+        Seat::Empty
+    }
+}
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 enum Seat {
@@ -64,10 +99,13 @@ impl Iterator for AdjacentIter {
     }
 }
 
-fn read_seats(input: &str) -> Seats {
+fn read_seats(input: &str) -> SeatMap {
     let mut seats = HashMap::new();
+    let mut width = 0;
+    let mut height = 0;
 
     for (y, line) in input.lines().enumerate() {
+        width = line.len();
         for (x, char) in line.chars().enumerate() {
             // insert returns Option type
             let _ = match char {
@@ -77,12 +115,17 @@ fn read_seats(input: &str) -> Seats {
                 c => unreachable!("Got unexpected char {}", c),
             };
         }
+        height = y;
     }
 
-    seats
+    SeatMap {
+        seats: seats,
+        width: width,
+        height: height,
+    }
 }
 
-fn iterate(seats: &Seats) -> Seats {
+fn iterate_adjacent(seats: &Seats) -> Seats {
     seats
         .iter()
         .map(|(&p, seat)| {
@@ -114,11 +157,62 @@ fn iterate(seats: &Seats) -> Seats {
         .collect()
 }
 
-fn find_fixed_point(seats: &Seats) -> Seats {
+fn iterate_path(seats: &SeatMap) -> SeatMap {
+    let new = seats
+        .seats
+        .iter()
+        .map(|(&p, seat)| {
+            let occupied = STEPS
+                .iter()
+                .filter(|x| seats.first_in_path(p, *x) == Seat::Occupied)
+                .count();
+
+            (
+                p,
+                match seat {
+                    Seat::Empty => {
+                        if occupied == 0 {
+                            Seat::Occupied
+                        } else {
+                            Seat::Empty
+                        }
+                    }
+                    Seat::Occupied => {
+                        if occupied >= 5 {
+                            Seat::Empty
+                        } else {
+                            Seat::Occupied
+                        }
+                    }
+                },
+            )
+        })
+        .collect();
+
+    SeatMap {
+        seats: new,
+        width: seats.width,
+        height: seats.height,
+    }
+}
+
+fn find_fixed_point_adjacent(seats: &Seats) -> Seats {
     let mut old: Seats = seats.to_owned();
 
     loop {
-        let new = iterate(&old);
+        let new = iterate_adjacent(&old);
+        if new == old {
+            return new;
+        }
+        old = new;
+    }
+}
+
+fn find_fixed_point_path(seats: &SeatMap) -> SeatMap {
+    let mut old: SeatMap = seats.to_owned();
+
+    loop {
+        let new = iterate_path(&old);
         if new == old {
             return new;
         }
@@ -127,7 +221,15 @@ fn find_fixed_point(seats: &Seats) -> Seats {
 }
 
 fn part1(seats: &Seats) -> usize {
-    find_fixed_point(&seats)
+    find_fixed_point_adjacent(&seats)
+        .iter()
+        .filter(|(_, &s)| s == Seat::Occupied)
+        .count()
+}
+
+fn part2(seats: &SeatMap) -> usize {
+    find_fixed_point_path(&seats)
+        .seats
         .iter()
         .filter(|(_, &s)| s == Seat::Occupied)
         .count()
@@ -137,7 +239,9 @@ fn main() {
     let input = fs::read_to_string("../inputs/day11.txt").unwrap();
     let seats = read_seats(&input);
 
-    println!("{}", part1(&seats));
+    println!("{}", part1(&seats.seats));
+
+    println!("{}", part2(&seats));
 }
 
 mod test {
@@ -148,7 +252,15 @@ mod test {
         let input = fs::read_to_string("../inputs/day11-test.txt").unwrap();
         let seats = read_seats(&input);
 
-        assert_eq!(part1(&seats), 37);
+        assert_eq!(part1(&seats.seats), 37);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = fs::read_to_string("../inputs/day11-test.txt").unwrap();
+        let seats = read_seats(&input);
+
+        assert_eq!(part2(&seats), 26);
     }
 
     #[test]
