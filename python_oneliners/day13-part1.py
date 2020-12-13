5b721c0a78b988ae488b6879ff9ef16224eac53b
Chris Oates <kryz.oatz@gmail.com>
2020-12-13 12:50:54 +0000

    day13: part 1, one liner

---
+++ python_oneliners/day13-part1.py
@@ -0,0 +1,1 @@
+print((lambda t, ids: min(((i - int(t)%i, i*(i - int(t)%i)) for i in (int(i) for i in ids.split(',') if i != 'x')), key=lambda x: x[0]))(*open('../inputs/day13.txt').read().splitlines())[1])
