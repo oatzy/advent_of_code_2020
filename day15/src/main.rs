e05f0dc52571bf749a2f00872271697157285cb1
Chris <kryz.oatz@gmail.com>
2020-12-15 11:21:37 +0000

    day15: part1
    Part2 doesn't work as is. I suspect it's one of those where there's eventually a pattern/repetition

---
+++ day15/src/main.rs
@@ -0,0 +1,56 @@
+use std::collections::HashMap;
+
+fn find_nth(prelude: &Vec<usize>, n: usize) -> usize {
+    let mut inx = prelude.len();
+    let mut current = *prelude.last().unwrap();
+
+    let mut last_seen: HashMap<usize, usize> = prelude[..inx - 1]
+        .iter()
+        .enumerate()
+        .map(|(i, v)| (*v, i + 1))
+        .collect();
+
+    while inx < n {
+        let new = match last_seen.get(&current) {
+            Some(x) => inx - x,
+            None => 0,
+        };
+
+        last_seen.insert(current, inx);
+
+        inx += 1;
+        current = new;
+    }
+
+    current
+}
+
+fn main() {
+    let input = vec![2, 0, 1, 7, 4, 14, 18];
+    println!("{}", find_nth(&input, 2020));
+}
+
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_part1_short() {
+        let prelude = &vec![0, 3, 6];
+        assert_eq!(find_nth(prelude, 10), 0);
+    }
+
+    #[test]
+    fn test_part1_long() {
+        assert_eq!(find_nth(&vec![1, 3, 2], 2020), 1);
+        assert_eq!(find_nth(&vec![2, 1, 3], 2020), 10);
+        assert_eq!(find_nth(&vec![1, 2, 3], 2020), 27);
+        assert_eq!(find_nth(&vec![2, 3, 1], 2020), 78);
+        assert_eq!(find_nth(&vec![3, 2, 1], 2020), 438);
+        assert_eq!(find_nth(&vec![3, 1, 2], 2020), 1836);
+    }
+
+    #[test]
+    fn test_part2_1() {
+        assert_eq!(find_nth(&vec![0, 3, 6], 30000000), 175594);
+    }
+}
