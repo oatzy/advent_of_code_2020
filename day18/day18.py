8a1902100e7dcd8a9107742c5805b5a09bbf4443
Chris Oates <kryz.oatz@gmail.com>
2020-12-18 22:38:00 +0000

    day18: part1, python tidy up

--- day18/day18.py
+++ day18/day18.py
@@ -1,23 +1,25 @@
 from operator import add, mul
 
+
 def _evaluate(string, inx):
     value = None
     op = None
-    
+
     while inx < len(string):
         c = string[inx]
+
         if c.isdigit():
             if value is None:
                 value = int(c)
             else:
                 value = op(value, int(c))
                 op = None
+
         elif c == '+':
             op = add
         elif c == '*':
             op = mul
-        elif c == ')':
-            break
+
         elif c == '(':
             v, inx = _evaluate(string, inx+1)
             if value is None:
@@ -25,30 +27,28 @@ def _evaluate(string, inx):
             else:
                 value = op(value, v)
                 op = None
-        elif c == ' ':
+
+        elif c in ')\n':
+            break
+        elif c in ' ':
             pass
         else:
             raise Exception(f"unknown char {c}")
-        #print(c, value, op)
+
         inx += 1
-     
+
     return value, inx
-                
+
+
 def evaluate(string):
-    result, inx = _evaluate(string, 0)
-    assert inx >= len(string), f"{string} => {inx} < {len(string)}"
-    return result
-    
+    return _evaluate(string, 0)[0]
+
 
 def main():
-    total = 0
-    with open('inputs/day18.txt', 'r') as f:
-        for line in f:
-            result = evaluate(line.strip())
-            print(f"{line.strip()} = {result}")
-            total += result
-    print(total)
-    
+    with open('../inputs/day18.txt', 'r') as f:
+        print(sum(evaluate(l) for l in f))
+
+
 if __name__ == '__main__':
     #print(evaluate("1 + 2 * 3 + 4 * 5 + 6"))
     main()
