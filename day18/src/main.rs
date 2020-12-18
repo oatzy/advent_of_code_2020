5bcaa46bf2291b4d7af8157ea0b117f4874ba528
Chris Oates <kryz.oatz@gmail.com>
2020-12-18 22:32:08 +0000

    day18: part1, rust
    Using nom, it's basically the same as part 2, just with the precendence removed

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -38,17 +38,54 @@ fn evaluate(string: &str) -> usize {
     r
 }
 
+fn factor1(string: &str) -> IResult<&str, usize> {
+    alt((digit, delimited(tag("("), expr1, tag(")"))))(string)
+}
+
+fn expr1(e: &str) -> IResult<&str, usize> {
+    let (s, init) = factor1(e).unwrap();
+    fold_many0(
+        tuple((alt((tag(" * "), tag(" + "))), factor1)),
+        init,
+        |acc, v| if v.0 == " * " { acc * v.1 } else { acc + v.1 },
+    )(s)
+}
+
+fn evaluate1(string: &str) -> usize {
+    let (s, r) = expr1(string).unwrap();
+    if s.len() > 0 {
+        panic!("unparsed {}", s);
+    }
+    r
+}
+
 fn main() {
     let input = fs::read_to_string("../inputs/day18.txt").unwrap();
-    let res: usize = input.lines().map(|s| evaluate(s)).sum();
-    println!("{}", res);
+    println!("{}", input.lines().map(|s| evaluate1(s)).sum::<usize>());
+    println!("{}", input.lines().map(|s| evaluate(s)).sum::<usize>());
 }
 
 mod test {
     use super::*;
 
     #[test]
+    fn test_part1() {
+        assert_eq!(evaluate1("1 + (2 * 3) + (4 * (5 + 6))"), 51);
+        assert_eq!(evaluate1("2 * 3 + (4 * 5)"), 26);
+        assert_eq!(evaluate1("5 + (8 * 3 + 9 + 3 * 4 * 3)"), 437);
+        assert_eq!(
+            evaluate1("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"),
+            12240
+        );
+        assert_eq!(
+            evaluate1("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"),
+            13632
+        );
+    }
+
+    #[test]
     fn test_part2() {
+        assert_eq!(evaluate("1 + 2 * 3 + 4 * 5 + 6"), 231);
         assert_eq!(evaluate("1 + (2 * 3) + (4 * (5 + 6))"), 51);
         assert_eq!(evaluate("2 * 3 + (4 * 5)"), 46);
         assert_eq!(evaluate("5 + (8 * 3 + 9 + 3 * 4 * 3)"), 1445);
