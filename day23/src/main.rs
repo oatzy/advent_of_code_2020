7721e95b6e14b26000ecaf2d3fcd6fb9abb5eaf7
Chris Oates <kryz.oatz@gmail.com>
2020-12-25 16:12:00 +0000

    day23: part2, with help
    I had to check the reddit for how to tackle this one, so there's a big old asterisk next to this star.
    
    Also much fun was had with mutable/immutable borrows

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -4,7 +4,6 @@ use std::fmt;
 struct Cups {
     cups: VecDeque<usize>,
     hand: [usize; 3],
-    round: usize,
     last_insert: usize,
 }
 
@@ -13,7 +12,6 @@ impl From<Vec<usize>> for Cups {
         Self {
             cups: input.into(),
             hand: [0; 3],
-            round: 0,
             last_insert: 0,
         }
     }
@@ -38,12 +36,6 @@ impl fmt::Display for Cups {
 }
 
 impl Cups {
-    fn extend_to(&mut self, n: usize) {
-        for i in (self.cups.len() + 1)..=n {
-            self.cups.push_back(i);
-        }
-    }
-
     fn play1(&mut self) {
         let current = *self.cups.front().unwrap();
 
@@ -77,7 +69,6 @@ impl Cups {
             }
         }
 
-        //println!("{}", position);
         for i in self.hand.iter() {
             self.cups.insert((position + 1) % self.cups.len(), *i)
         }
@@ -86,124 +77,122 @@ impl Cups {
     }
 
     fn playn(&mut self, n: usize) {
-        for i in 0..n {
-            //println!("round {}", i);
-            if i % 10_000 == 0 {
-                println!("round {}", i);
-                println!("{}", self.last_insert);
-                // println!("{:?}", self.hand);
-                // println!("{:?}", self.cups.iter().take(10).collect::<Vec<_>>());
-                println!("{:?}", self.find_stars());
-            }
+        for _ in 0..n {
             self.play1();
         }
     }
+}
 
-    fn find_stars(&self) -> (usize, usize) {
-        let mut inx = 0;
-        while self.cups[inx] != 1 {
-            inx += 1;
+/// doubly linked list, implemented as a Vec
+/// Concept stolen from the advent of code reddit.
+
+struct Cup {
+    label: usize,
+    prev_inx: usize,
+    next_inx: usize,
+}
+
+struct LLCups {
+    cups: Vec<Cup>,
+    cur_inx: usize,
+    size: usize,
+}
+
+impl LLCups {
+    fn with_size(n: usize) -> Self {
+        let mut cups = Vec::with_capacity(n);
+
+        for i in 0..n {
+            cups.push(Cup {
+                label: i + 1,
+                next_inx: i + 1,
+                prev_inx: i,
+            })
         }
-        println!("{}", inx);
-        (
-            self.cups[(inx + 1) % self.cups.len()],
-            self.cups[(inx + 2) % self.cups.len()],
-        )
-    }
-}
 
-struct VecCups {
-    cups: Vec<usize>,
-    hand: [usize; 3],
-    head: usize,
-}
+        cups[0].prev_inx = n - 1;
+        cups[n - 1].next_inx = 0;
 
-impl fmt::Display for VecCups {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut i = 0;
-        while self.cups[i] != 1 {
-            i = (i + 1) % self.cups.len();
-        }
-        i = (i + 1) % self.cups.len();
-        let values = self
-            .cups
-            .iter()
-            .skip(i)
-            .chain(self.cups.iter().take(if i == 0 { 0 } else { i - 1 }))
-            .map(|x| x.to_string())
-            .collect::<Vec<String>>()
-            .join("");
-
-        write!(f, "{}", values)
-    }
-}
-
-impl VecCups {
-    fn new() -> Self {
         Self {
-            cups: Vec::new(),
-            hand: [0, 0, 0],
-            head: 0,
+            cups: cups,
+            cur_inx: 0,
+            size: n,
         }
+    }
+
+    fn next_three_contains(&self, inx: usize, label: usize) -> bool {
+        let mut inx = inx;
+        for _ in 0..3 {
+            let cup = self.cups.get(inx).unwrap();
+            if cup.label == label {
+                return true;
+            }
+            inx = cup.next_inx;
+        }
+        false
+    }
+
+    fn find_cup(&self, label: usize) -> usize {
+        // everything after the first 10 is label == inx + 1
+        if label >= 10 {
+            return label - 1;
+        }
+
+        for i in 0..10 {
+            let cup = self.cups.get(i).unwrap();
+            if cup.label == label {
+                return i;
+            }
+        }
+        panic!("cup with label {} wasn't found", label);
     }
 
     fn play1(&mut self) {
-        let current = self.cups[self.head];
+        let (current, hand_inx) = {
+            let current = self.cups.get(self.cur_inx).unwrap();
+            (current.label, current.next_inx)
+        };
 
-        let len = self.cups.len();
-        let mut offset = (self.head + 1) % len;
-
-        // take 3 after current
-        for i in 0..3 {
-            self.hand[i] = self.cups[(offset + i) % len];
+        let mut dest = if current > 1 { current - 1 } else { self.size };
+        while self.next_three_contains(hand_inx, dest) {
+            dest = if dest > 1 { dest - 1 } else { self.size };
         }
 
-        // find current - 1 (or next available)
-        let mut dest = if current > 1 { current - 1 } else { len };
-        while self.hand.contains(&dest) {
-            dest = if dest > 1 { dest - 1 } else { len };
-        }
+        let dest_inx = self.find_cup(dest);
 
-        // shift everything right 3 until we find dest
-        while offset != self.head {
-            self.cups[offset] = self.cups[(offset + 3) % len];
-            if self.cups[offset] == dest {
-                break;
-            }
-            offset = (offset + 1) % len;
-        }
-        assert!(offset != self.head);
+        let (tail_inx, tail_next) = {
+            let mid_inx = self.cups.get(hand_inx).unwrap().next_inx;
+            let tail_inx = self.cups.get(mid_inx).unwrap().next_inx;
+            let tail_next = self.cups.get(tail_inx).unwrap().next_inx;
+            (tail_inx, tail_next)
+        };
 
-        // insert hand after dest
-        for (i, x) in self.hand.iter().enumerate() {
-            self.cups[(offset + i + 1) % len] = *x;
-        }
+        self.cups.get_mut(self.cur_inx).unwrap().next_inx = tail_next;
+        self.cups.get_mut(tail_next).unwrap().prev_inx = self.cur_inx;
 
-        // move to one after current
-        self.head = (self.head + 1) % len;
+        let dest_next = self.cups.get(dest_inx).unwrap().next_inx;
+        self.cups.get_mut(tail_inx).unwrap().next_inx = dest_next;
+        self.cups.get_mut(dest_next).unwrap().prev_inx = tail_inx;
+
+        self.cups.get_mut(dest_inx).unwrap().next_inx = hand_inx;
+        self.cups.get_mut(hand_inx).unwrap().prev_inx = dest_inx;
+
+        self.cur_inx = self.cups.get(self.cur_inx).unwrap().next_inx;
     }
 
     fn playn(&mut self, n: usize) {
-        for i in 0..n {
-            if i % 10_000 == 0 {
-                println!("round {}", i);
-                // println!("{:?}", self.hand);
-                // println!("{:?}", self.cups.iter().take(10).collect::<Vec<_>>());
-                println!("{:?}", self.find_stars());
-            }
+        for _ in 0..n {
             self.play1();
         }
     }
 
     fn find_stars(&self) -> (usize, usize) {
-        let mut inx = 0;
-        while self.cups[inx] != 1 {
-            inx += 1;
-        }
-        println!("{}", inx);
+        let oneinx = self.find_cup(1);
+        let one_cup = self.cups.get(oneinx).unwrap();
+        let next_cup = self.cups.get(one_cup.next_inx).unwrap();
         (
-            self.cups[(inx + 1) % self.cups.len()],
-            self.cups[(inx + 2) % self.cups.len()],
+            next_cup.label,
+            self.cups.get(next_cup.next_inx).unwrap().label,
         )
     }
 }
@@ -211,23 +200,24 @@ impl VecCups {
 fn main() {
     // part1
     {
-        let mut cups: VecCups = VecCups::new();
-        cups.cups.extend(vec![9, 6, 2, 7, 1, 3, 8, 5, 4]);
+        let mut cups: Cups = vec![9, 6, 2, 7, 1, 3, 8, 5, 4].into();
         cups.playn(100);
         println!("{}", cups);
     }
 
     // part2
-    // {
-    //     let mut cups: Cups = vec![9, 6, 2, 7, 1, 3, 8, 5, 4].into();
-    //     cups.extend_to(1_000_000);
+    {
+        let mut cups = LLCups::with_size(1_000_000);
+        for (i, x) in vec![9, 6, 2, 7, 1, 3, 8, 5, 4].iter().cloned().enumerate() {
+            cups.cups.get_mut(i).unwrap().label = x;
+        }
 
-    //     cups.playn(10_000_000);
-    //     let stars = cups.find_stars();
+        cups.playn(10_000_000);
+        let stars = cups.find_stars();
 
-    //     println!("{:?}", stars);
-    //     println!("{}", stars.0 * stars.1);
-    // }
+        println!("{:?}", stars);
+        println!("{}", stars.0 * stars.1);
+    }
 }
 
 #[cfg(test)]
@@ -246,34 +236,11 @@ mod test {
     }
 
     #[test]
-    fn test_part1_vec() {
-        let mut cups = VecCups::new();
-        cups.cups.extend(vec![3, 8, 9, 1, 2, 5, 4, 6, 7]);
-
-        cups.playn(10);
-        assert_eq!(format!("{}", cups), "92658374");
-
-        cups.playn(90);
-        assert_eq!(format!("{}", cups), "67384529");
-    }
-
-    #[test]
     fn test_part2() {
-        let mut cups: Cups = vec![3, 8, 9, 1, 2, 5, 4, 6, 7].into();
-        cups.extend_to(1_000_000);
-
-        cups.playn(10_000_000);
-        let stars = cups.find_stars();
-
-        assert_eq!(stars, (934001, 159792));
-        assert_eq!(stars.0 * stars.1, 149245887792);
-    }
-
-    #[test]
-    fn test_part2_vec() {
-        let mut cups = VecCups::new();
-        cups.cups.extend(vec![3, 8, 9, 1, 2, 5, 4, 6, 7]);
-        cups.cups.extend(10..=1_000_000);
+        let mut cups = LLCups::with_size(1_000_000);
+        for (i, x) in vec![3, 8, 9, 1, 2, 5, 4, 6, 7].iter().cloned().enumerate() {
+            cups.cups.get_mut(i).unwrap().label = x;
+        }
 
         cups.playn(10_000_000);
         let stars = cups.find_stars();
